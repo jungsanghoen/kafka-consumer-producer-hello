c964ab92b42c658960f345f254dbfaf557890d50
jung <jusahe@hotmail.com>
2025-07-25 15:14:02 +0900

    first init

---
+++ KafkaProducer/src/test/java/kr/or/exmaple/kafkaConsumeProducer/KafkaConsumeProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.exmaple.kafkaConsumeProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumeProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
